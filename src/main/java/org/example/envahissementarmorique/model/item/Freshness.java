361721ae7b92a267c86b04a40274e385cd011ab3
BOUDHIB-Mohame-Amine-24006551 <mohamed-amine.boudhib@etu.univ-amu.fr>
2025-11-21 15:11:19 +0100

    feat(freshness) : Creation of the Freshness class that verifies and degrade the freshness of a food

---
+++ src/main/java/org/example/envahissementarmorique/model/item/Freshness.java
@@ -0,0 +1,35 @@
+package org.example.envahissementarmorique.model.item;
+
+/**
+ * Defines the freshness state of a food item.
+ * - Fresh
+ * - Quite Fresh
+ * - Rotten
+ */
+
+public enum Freshness {
+    FRESH("Fresh"),
+    OKAY("Quite Fresh"),
+    ROTTEN("Rotten");
+
+    private final String label;
+    Freshness(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * Advances the decay process of the food.
+     * FRESH -> QUITE_FRESH -> ROTTEN -> ROTTEN
+     * * @return The next state of freshness.
+     */
+    public Freshness degrade() {
+        return switch(this) {
+            case FRESH -> OKAY;
+            case OKAY , ROTTEN -> ROTTEN;
+        };
+    }
+}
