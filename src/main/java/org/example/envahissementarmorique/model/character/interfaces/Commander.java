b1f60c2eda91e0c49a6d325f3918bc573fef2166
chetiouiwillem <willem.chetioui@etu.univ-amu.fr>
2025-11-21 16:38:43 +0100

    add : Ajout des méthodes dans l'interface pour les personnages Commander

--- src/main/java/org/example/envahissementarmorique/model/character/interfaces/Commander.java
+++ src/main/java/org/example/envahissementarmorique/model/character/interfaces/Commander.java
@@ -1,4 +1,17 @@
 package org.example.envahissementarmorique.model.character.interfaces;
 
 public class Commander {
+    void inspectLocation(){}
+
+    void createNewCharacter(){}
+
+    void healCharacter(){}
+
+    void feedCharacter(){}
+
+    void askToCreatePotion(){}
+
+    void drinkPotion(){}
+
+    void transferCharacterToLocation(){}
 }
