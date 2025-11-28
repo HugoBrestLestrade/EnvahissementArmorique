85ae8ce0d1ff5a8bd6723e740047a6c2c76e2fa4
chetiouiwillem <willem.chetioui@etu.univ-amu.fr>
2025-11-28 16:11:35 +0100

    fix : correction de l'erreur de compréhension du TD

--- src/main/java/org/example/envahissementarmorique/model/character/interfaces/Commander.java
+++ src/main/java/org/example/envahissementarmorique/model/character/interfaces/Commander.java
@@ -1,17 +1,5 @@
 package org.example.envahissementarmorique.model.character.interfaces;
 
 public interface Commander {
-    void inspectLocation();
-
-    void createNewCharacter();
-
-    void healCharacter();
-
-    void feedCharacter();
-
-    void askToCreatePotion();
-
-    void drinkPotion();
-
-    void transferCharacterToLocation();
+    void command();
 }
