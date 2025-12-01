4a5ea5c62ddc7a2a40f6b6df2a65dd60695212b5
chetiouiwillem <willem.chetioui@etu.univ-amu.fr>
2025-12-01 15:07:31 +0100

    feat : ajout des attributs des méthodes de la classe ClanLeader

---
+++ src/main/java/org/example/envahissementarmorique/model/character/base/ClanLeader.java
@@ -0,0 +1,39 @@
+package org.example.envahissementarmorique.model.character.base;
+
+import java.util.ArrayList;
+
+public class ClanLeader {
+
+    private String name;
+    private String genre;
+    private int age;
+    private Place place;
+
+    public void examinePlace() {
+
+    }
+
+    public void createCharacter(String name, String genre, String faction, double height, int age, int strength, int endurance, int health, int hunger, int belligerence, int magicPotion){
+
+    }
+
+    public void healTeam(ArrayList<Character> characters, int hpHealed){
+
+    }
+
+    public void feedTeam(ArrayList<Character> characters, int foodRestaured){
+
+    }
+
+    public void askToMakePotion(Druid druid){
+
+    }
+
+    public void drinkMagicPotion(Potion potion, ArrayList<Character> characters){
+
+    }
+
+    public void transferCharacter(Place place, ArrayList<Character> characters){
+
+    }
+}
